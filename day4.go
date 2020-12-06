f0290cb98d184a665d732f3f463edbe81ca32fbe
Fanny Rottee <frottee@vivlio.com>
2020-12-06 15:43:23 +0100

    day 4 part 2

--- day4.go
+++ day4.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -12,23 +14,106 @@ func main() {
 	fmt.Println(len(passports))
 	count := 0
 	for _, passport := range passports {
-		if (len(passport)) >= 8 {
-			count++
+		if (len(passport)) < 7 {
+
 			continue
 		}
 
-		_, ok := passport["cid"]
-		if ((len(passport)) == 7) && !(ok) {
-			count++
+		byr, ok := passport["byr"]
+		if !(ok && isValueValid(byr, 1920, 2002)) {
+
 			continue
 		}
 
+		iyr, ok := passport["iyr"]
+		if !(ok && isValueValid(iyr, 2010, 2020)) {
+
+			continue
+		}
+
+		eyr, ok := passport["eyr"]
+		if !(ok && isValueValid(eyr, 2020, 2030)) {
+
+			continue
+		}
+
+		hgt, ok := passport["hgt"]
+		if !ok {
+
+			continue
+		}
+		last2 := hgt[len(hgt)-2:]
+		size := hgt[0 : len(hgt)-2]
+		if last2 == "cm" {
+			if !isValueValid(size, 150, 193) {
+				continue
+			}
+		} else if last2 == "in" {
+			if !isValueValid(size, 59, 76) {
+				continue
+			}
+		} else {
+			continue
+		}
+
+		hcl, ok := passport["hcl"]
+		if !ok {
+
+			continue
+		}
+		matched, err := regexp.Match(`#([0-9]|[a-f]){6}`, []byte(hcl))
+
+		if !matched || err != nil {
+			continue
+		}
+
+		ecl, ok := passport["ecl"]
+		if !(ok && isValidEyeColor(ecl)) {
+			continue
+		}
+
+		pid, ok := passport["pid"]
+
+		if !(ok && len(pid) == 9) {
+
+			continue
+		}
+
+		matchedPid, err := regexp.Match(`[0-9]{9}`, []byte(pid))
+
+		if !matchedPid || err != nil {
+			continue
+		}
+
+		count++
 	}
 
 	fmt.Println(count)
 }
 
-func extractPassports() []map[string]interface{} {
+func isValidEyeColor(color string) bool {
+	validColors := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+	for _, validColor := range validColors {
+		if color == validColor {
+			return true
+		}
+	}
+	return false
+}
+
+func isValueValid(val string, min int, max int) bool {
+	i, err := strconv.Atoi(val)
+	if err != nil {
+		return false
+	}
+
+	if i < min || i > max {
+		return false
+	}
+	return true
+}
+
+func extractPassports() []map[string]string {
 	fileBytes, err := ioutil.ReadFile("input/day4")
 	if err != nil {
 		fmt.Println(err)
@@ -36,7 +121,7 @@ func extractPassports() []map[string]interface{} {
 	}
 
 	lines := strings.Split(string(fileBytes), "\n\n")
-	passports := make([]map[string]interface{}, 0)
+	passports := make([]map[string]string, 0)
 
 	for _, line := range lines {
 		var passportLine []string
@@ -45,7 +130,7 @@ func extractPassports() []map[string]interface{} {
 			passportLine = append(passportLine, strings.Split(halfLine, " ")...)
 		}
 
-		passport := make(map[string]interface{})
+		passport := make(map[string]string)
 		for _, element := range passportLine {
 			field := strings.Split(element, ":")
 			passport[field[0]] = field[1]
