e31f021e6ee1f462b7b55b9b3a7c9d57d26d4c58
Fanny Rottee <frottee@vivlio.com>
2020-12-10 13:41:53 +0100

    day 5 part 1

---
+++ day5.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	boardingPasses := extractInput()
+	highestSeatId := 0
+	for _, boardingPass := range boardingPasses {
+		row := findPosition([]int{0, 127}, boardingPass[0:7])
+		column := findPosition([]int{0, 7}, boardingPass[7:10])
+
+		seatId := (row * 8) + column
+
+		if seatId > highestSeatId {
+			highestSeatId = seatId
+		}
+	}
+
+	fmt.Println("Result")
+	fmt.Println(highestSeatId)
+}
+
+func findPosition(rangeEl []int, instructions string) int {
+	for _, instruction := range instructions {
+		instruction := string(instruction)
+		if instruction == "F" || instruction == "L" {
+			rangeEl[1] = rangeEl[0] + ((rangeEl[1] - rangeEl[0]) / 2)
+		} else if instruction == "B" || instruction == "R" {
+			rangeEl[0] = rangeEl[0] + ((rangeEl[1]-rangeEl[0])/2 + 1)
+		}
+	}
+
+	if rangeEl[0] != rangeEl[1] {
+		fmt.Println("Oh no")
+		os.Exit(1)
+	}
+
+	return rangeEl[0]
+}
+
+func extractInput() []string {
+	fileBytes, err := ioutil.ReadFile("input/day5")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return strings.Split(string(fileBytes), "\n")
+}
